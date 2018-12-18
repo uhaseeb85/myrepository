31546b7a743c97d67911cb760e44dfeb7af58a7d
uhaseeb85 <uhaseeb85@gmail.com>
2018-12-18 23:32:35 +0530

    New Project

---
+++ src/com/test/sample/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test.sample;
+
+/**
+ * @author Haseeb
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
